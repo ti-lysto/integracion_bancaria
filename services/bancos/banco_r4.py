be706e3830b0424776a7100e0f0cff3e97c32ff9
alirubior <alirubio@gmail.com>
2026-03-02 19:06:33 -0400

    fix: aprametros

--- services/bancos/banco_r4.py
+++ services/bancos/banco_r4.py
@@ -454,8 +454,8 @@ class R4Services:
                     "OTP": otp,
                     "Respuesta":data.get("message", ""),
                     "endpoint": "DebitoInmediato",
-                    #"id_dev_cred": data.get("id"),
-                    "id_dev_cred": "6785d97e-2092-49f0-9f7d-3d5921f0b136",
+                    "id_dev_cred": data.get("id"),
+                    #"id_dev_cred": "6785d97e-2092-49f0-9f7d-3d5921f0b136",
                     "Referencia": data.get("reference"),
                     #"Referencia": "REFerenciatest4",
                     "CodigoRed": data.get("code", "")
